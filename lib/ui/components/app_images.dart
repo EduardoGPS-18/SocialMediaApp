e3b43e4535f923d18049a01072f40f19e373df6b
Guilherme Eler <50060454+GuiEler@users.noreply.github.com>
2021-10-02 15:21:43 -0300

    fix: removed old image paths

--- lib/ui/components/app_images.dart
+++ lib/ui/components/app_images.dart
@@ -1,4 +1,1 @@
-class AppImages {
-  static String get splashLogo => "lib/ui/assets/images/splash_logo.png";
-  static String get logo => "lib/ui/assets/images/nerdflix_logo.png";
-}
+class AppImages {}
