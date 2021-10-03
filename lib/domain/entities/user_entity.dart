5b46ea26ad7b66e8937a027e628ca5462170f0a6
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-03 15:10:05 -0300

    refactor: remove token field from user entity

--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -2,11 +2,9 @@ class UserEntity {
   final String name;
   final String email;
   final String photoUrl;
-  final String token;
   UserEntity({
     required this.name,
     required this.email,
     required this.photoUrl,
-    required this.token,
   });
 }
