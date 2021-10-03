2806bf7f8c2a0710c629e4e11d25ea4b689e7309
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-03 15:05:53 -0300

    feat: added email field to user entity

--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -1,9 +1,11 @@
 class UserEntity {
   final String name;
+  final String email;
   final String photoUrl;
   final String token;
   UserEntity({
     required this.name,
+    required this.email,
     required this.photoUrl,
     required this.token,
   });
