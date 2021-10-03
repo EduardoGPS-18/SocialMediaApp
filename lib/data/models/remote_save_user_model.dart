67b87671c7d0227fd3acfbe54d0184df8d11555f
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-03 16:04:09 -0300

    feat: added toMap method to remote saver user model

--- lib/data/models/remote_save_user_model.dart
+++ lib/data/models/remote_save_user_model.dart
@@ -6,4 +6,10 @@ class RemoteSaveUserModel extends UserEntity {
     required String photoUrl,
     required String email,
   }) : super(name: name, photoUrl: photoUrl, email: email);
+
+  Map<String, String> toMap() => {
+        "name": name,
+        "photoUrl": photoUrl,
+        "email": email,
+      };
 }
