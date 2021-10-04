2cd72e703121a3f49171702ecf37208596fade03
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-04 16:37:16 -0300

    feat: create getx auth presenter factory

---
+++ lib/main/factory/pages/auth/getx_auth_presenter_factory.dart
@@ -0,0 +1,8 @@
+import '../../../../presentation/presenters/auth/auth.dart';
+import '../../../../ui/pages/pages.dart';
+import '../../factory.dart';
+
+AuthPagePresenter makeAuthPagePresenter() => GetxAuthPagePresenter(
+      remoteLoginUser: makeLoginUser(),
+      remoteRegisterUser: makeRegisterUser(),
+    );
