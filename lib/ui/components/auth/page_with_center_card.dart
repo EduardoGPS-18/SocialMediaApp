c1b459c7ee6ce13a0b2c6cf75198b38450583bb6
Guilherme Eler <50060454+GuiEler@users.noreply.github.com>
2021-10-04 20:07:33 -0300

    refactor: android back button works on pageview

--- lib/ui/components/auth/page_with_center_card.dart
+++ lib/ui/components/auth/page_with_center_card.dart
@@ -3,44 +3,54 @@ import 'package:flutter/material.dart';
 class PageWithCenterCard extends StatelessWidget {
   final Size size;
   final List<Widget> children;
+  final PageController controller;
 
-  const PageWithCenterCard({Key? key, required this.size, required this.children}) : super(key: key);
+  const PageWithCenterCard({
+    Key? key,
+    required this.size,
+    required this.children,
+    required this.controller,
+  }) : super(key: key);
+
+  bool onWillPop() {
+    if (controller.page?.round() == controller.initialPage) {
+      return true;
+    } else {
+      controller.previousPage(
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.linear,
+      );
+      return false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: size.width * 0.05,
-      right: size.width * 0.05,
-      bottom: size.height * 0.05,
-      top: size.height * 0.05,
-      child: SingleChildScrollView(
-        child: SizedBox(
-          height: size.height,
-          child: Center(
-            child: Container(
-              height: size.height * 0.9,
-              width: size.width * 0.9,
-              decoration: BoxDecoration(
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 25.0,
-                    spreadRadius: 1.0,
-                    offset: Offset(
-                      2.0,
-                      2.0,
-                    ),
-                  ),
-                ],
-                borderRadius: BorderRadius.circular(size.width * 0.015),
-                color: Theme.of(context).colorScheme.background,
-              ),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: children,
+    return WillPopScope(
+      onWillPop: () => Future.sync(onWillPop),
+      child: Center(
+        child: Container(
+          height: size.height * 0.9,
+          width: size.width * 0.9,
+          decoration: BoxDecoration(
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 25.0,
+                spreadRadius: 1.0,
+                offset: Offset(
+                  2.0,
+                  2.0,
                 ),
               ),
+            ],
+            borderRadius: BorderRadius.circular(size.width * 0.015),
+            color: Theme.of(context).colorScheme.background,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: children,
             ),
           ),
         ),
