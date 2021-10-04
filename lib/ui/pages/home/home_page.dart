80e37d1853e5a3840ac4982bc848e51780d28c8c
Pabricio Freitas <pabricio2000@gmail.com>
2021-10-04 19:53:37 -0300

    feat: addad Bottom Navigation Bar

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -10,10 +10,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late PageController pageController;
+
+  bool islog = true;
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: 0);
+  }
+
+  int _currentIndexBottomNavigationBar = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // backgroundColor: Theme.of(context).colorScheme.primary,
-        appBar: CustomAppBar(text: "Feed"));
+      // backgroundColor: Theme.of(context).colorScheme.primary,
+      appBar: CustomAppBar(text: "Feed"),
+
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 15,
+        backgroundColor: Theme.of(context).colorScheme.background,
+        onTap: (index) {
+          setState(() {
+            _currentIndexBottomNavigationBar = index;
+          });
+        },
+        currentIndex: _currentIndexBottomNavigationBar,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Feed',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            label: 'Postar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Perfil',
+          ),
+        ],
+      ),
+    );
   }
 }
