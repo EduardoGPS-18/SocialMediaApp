b0a95587c84f103568baf540fca6fb281fd2eaad
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-05 03:04:49 -0300

    feat: remove deleted validator

--- lib/main/factory/pages/auth/auth_validation_factory.dart
+++ lib/main/factory/pages/auth/auth_validation_factory.dart
@@ -5,9 +5,9 @@ import '../../../composites/composites.dart';
 
 Validation makeAuthValidation() => ValidationComposite(validations: makeAuthValidations());
 List<FieldValidation> makeAuthValidations() => [
-      ...ValidationBuilder.field('email').notNullable().required().email().build(),
-      ...ValidationBuilder.field('password').notNullable().required().min(8).build(),
-      ...ValidationBuilder.field('confirm_password').notNullable().required().min(8).sameAs('password').build(),
-      ...ValidationBuilder.field('image').notNullable().required().build(),
-      ...ValidationBuilder.field('name').notNullable().required().min(8).build(),
+      ...ValidationBuilder.field('email').required().email().build(),
+      ...ValidationBuilder.field('password').required().min(8).build(),
+      ...ValidationBuilder.field('confirm_password').required().min(8).sameAs('password').build(),
+      ...ValidationBuilder.field('image').required().build(),
+      ...ValidationBuilder.field('name').required().min(8).build(),
     ];
