4df7bf63bbdd164ecbdd30ce9b6b32547aee36bb
Pabricio Freitas <pabricio2000@gmail.com>
2021-10-05 15:54:59 -0300

    feat: widget to view posts

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool islog = true;
-
+  bool onPressedFavorite = false;
   int _currentIndexBottomNavigationBar = 0;
   @override
   Widget build(BuildContext context) {
@@ -41,42 +41,147 @@ class _HomePageState extends State<HomePage> {
               ListView.builder(
                   primary: false,
                   shrinkWrap: true,
-                  itemCount: 1,
+                  itemCount: 3,
                   itemBuilder: (context, index) {
                     return Column(children: [
                       SizedBox(
-                        height: 230,
+                        height: size.height * 0.33,
                         //color: Colors.grey,
-                        child: Column(
-                          children: [
-                            ListTile(
-                              leading: GestureDetector(
-                                onTap: () {},
-                                child: Container(
-                                  width: size.width * 0.16,
-                                  height: size.width * 0.16,
-                                  decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      image: DecorationImage(
-                                        fit: BoxFit.cover,
-                                        image: AssetImage(
-                                            "lib/ui/assets/images/test.jpg"),
-                                      )),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: size.height * 0.02,
+                          ),
+                          child: Container(
+                            //   color: Colors.red,
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  leading: InkWell(
+                                    onTap: () {},
+                                    child: Container(
+                                      width: size.width * 0.16,
+                                      height: size.width * 0.16,
+                                      decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          image: DecorationImage(
+                                            fit: BoxFit.cover,
+                                            image: AssetImage(
+                                                "lib/ui/assets/images/test.jpg"),
+                                          )),
+                                    ),
+                                  ),
+                                  title: Padding(
+                                    padding: EdgeInsets.only(
+                                        bottom: size.width * 0.015),
+                                    child: Text(
+                                      "Pabricio Freitas",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  subtitle: Text("Terça-feira ás 08:22"),
                                 ),
-                              ),
-                              title: Padding(
-                                padding:
-                                    EdgeInsets.only(bottom: size.width * 0.02),
-                                child: Text(
-                                  "Pabricio Freitas",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: size.height * 0.005,
+                                    horizontal: size.width * 0.045,
+                                  ),
+                                  child: InkWell(
+                                    onTap: () {},
+                                    child: Text(
+                                      "Mais um trabalho realizado com sucesso. Obrigado ao aplicativo por proporcionar essa oportunidade. ",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                      ),
+                                      maxLines: 3,
+                                      overflow: TextOverflow.ellipsis,
+                                      textAlign: TextAlign.left,
+                                    ),
+                                  ),
                                 ),
-                              ),
-                              subtitle: Text("Pabricio Freitas"),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: size.width * 0.015,
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      IconButton(
+                                        iconSize: size.width * 0.07,
+                                        onPressed: () {},
+                                        icon: onPressedFavorite
+                                            ? Icon(
+                                                Icons.favorite_outlined,
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .error,
+                                              )
+                                            : Icon(
+                                                Icons.favorite_border_outlined,
+                                              ),
+                                      ),
+                                      InkWell(
+                                        onTap: () {},
+                                        child: Text.rich(
+                                          TextSpan(
+                                            text: "40",
+                                            style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .primary,
+                                            ),
+                                            children: [
+                                              TextSpan(
+                                                text: " curtidas",
+                                                style: TextStyle(
+                                                  color: Theme.of(context)
+                                                      .colorScheme
+                                                      .onBackground,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      //
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                          left: size.width * 0.05,
+                                        ),
+                                        child: IconButton(
+                                            iconSize: size.width * 0.07,
+                                            onPressed: () {},
+                                            icon: Icon(
+                                                Icons.mode_comment_outlined)),
+                                      ),
+                                      InkWell(
+                                        onTap: () {},
+                                        child: Text.rich(
+                                          TextSpan(
+                                            text: "40",
+                                            style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .primary,
+                                            ),
+                                            children: [
+                                              TextSpan(
+                                                text: " comentários",
+                                                style: TextStyle(
+                                                  color: Theme.of(context)
+                                                      .colorScheme
+                                                      .onBackground,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
                             ),
-                            Text(
-                                "Mais um trabalho realizado com sucesso. Obrigado ao aplicativo por proporcionar essa oportunidade")
-                          ],
+                          ),
                         ),
                       ),
                       CustomDivider(
