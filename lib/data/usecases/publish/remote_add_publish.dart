48710e0f970b6941d7680806b59728a856736243
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-05 16:01:37 -0300

    feat: added remote add publish

--- lib/data/usecases/publish/remote_add_publish.dart
+++ lib/data/usecases/publish/remote_add_publish.dart
@@ -1,9 +1,32 @@
-import '../../../domain/entities/publish_entity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../../domain/entities/entities.dart';
 import '../../../domain/usecases/usecases.dart';
+import '../../firebase/firebase.dart';
+import '../../models/models.dart';
 
 class RemoteAddPublish implements AddPublish {
+  FirebaseCloudFirestore firebaseCloudFirestore;
+
+  RemoteAddPublish({
+    required this.firebaseCloudFirestore,
+  });
+
   @override
-  Future<void> addPublish({required PublishEntity publish}) {
-    throw UnimplementedError();
+  Future<void> addPublish({required PublishEntity publish}) async {
+    try {
+      final response = await firebaseCloudFirestore
+          .getCollection(
+            collectionName: 'publishes',
+          )
+          .doc(publish.userId)
+          .collection("publishes")
+          .add(
+            RemotePublishModel.fromEntity(publish).toMap(),
+          );
+      response.update({'uid': response.id});
+    } on FirebaseAuthException catch (_) {
+      rethrow;
+    }
   }
 }
