a72f14d7d57b2f31ef18588625ccf1debb1de9f0
Pabricio Freitas <pabricio2000@gmail.com>
2021-10-05 17:36:31 -0300

    feat: added shimmer in view post

--- lib/shared/widget/view_post.dart
+++ lib/shared/widget/view_post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 import 'widget.dart';
 
@@ -15,7 +16,7 @@ class ViewPost extends StatefulWidget {
 
 class _ViewPostState extends State<ViewPost> {
   bool onPressedFavorite = false;
-
+  String testSmimer = "a";
   @override
   Widget build(BuildContext context) {
     return Column(children: [
@@ -28,129 +29,193 @@ class _ViewPostState extends State<ViewPost> {
           ),
           child: Container(
             //   color: Colors.red,
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: widget.size.width * 0.01),
-                  child: ListTile(
-                    leading: InkWell(
-                      onTap: () {},
-                      child: Container(
-                        width: widget.size.width * 0.13,
-                        height: widget.size.width * 0.13,
-                        decoration: BoxDecoration(
+            child: testSmimer.isEmpty
+                ? Shimmer.fromColors(
+                    baseColor: Theme.of(context)
+                        .colorScheme
+                        .onBackground
+                        .withAlpha(60),
+                    highlightColor: Colors.grey[100]!,
+                    child: Column(children: [
+                      ListTile(
+                        leading: Container(
+                          width: widget.size.width * 0.13,
+                          height: widget.size.width * 0.13,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
                             shape: BoxShape.circle,
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image:
-                                  AssetImage("lib/ui/assets/images/test.jpg"),
-                            )),
-                      ),
-                    ),
-                    title: Padding(
-                      padding:
-                          EdgeInsets.only(bottom: widget.size.width * 0.018),
-                      child: Text(
-                        "Pabricio Freitas",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    subtitle: Text("Terça-feira ás 08:22"),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: widget.size.height * 0.005,
-                    horizontal: widget.size.width * 0.05,
-                  ),
-                  child: InkWell(
-                    onTap: () {},
-                    child: Text(
-                      "Mais um trabalho realizado com sucesso. Obrigado ao aplicativo por proporcionar essa oportunidade. ",
-                      style: TextStyle(
-                        fontSize: 18,
-                      ),
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: widget.size.width * 0.015,
-                  ),
-                  child: Row(
-                    children: [
-                      IconButton(
-                        iconSize: widget.size.width * 0.07,
-                        onPressed: () {},
-                        icon: onPressedFavorite
-                            ? Icon(
-                                Icons.favorite_outlined,
-                                color: Theme.of(context).colorScheme.error,
-                              )
-                            : Icon(
-                                Icons.favorite_border_outlined,
-                              ),
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: Text.rich(
-                          TextSpan(
-                            text: "40",
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                            children: [
-                              TextSpan(
-                                text: " curtidas",
-                                style: TextStyle(
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onBackground,
-                                ),
-                              ),
-                            ],
                           ),
                         ),
+                        title: Container(
+                          color: Colors.red,
+                          height: widget.size.width * 0.04,
+                          width: widget.size.width * 0.04,
+                        ),
+                        subtitle: Container(
+                          color: Colors.red,
+                          height: widget.size.width * 0.04,
+                          width: widget.size.width * 0.04,
+                        ),
                       ),
-                      //
+                      Container(
+                        color: Colors.red,
+                        height: widget.size.height * 0.13,
+                        width: widget.size.width * 0.91,
+                      ),
+                      Container(
+                        height: 10,
+                      ),
                       Padding(
-                        padding: EdgeInsets.only(
-                          left: widget.size.width * 0.05,
-                        ),
-                        child: IconButton(
-                            iconSize: widget.size.width * 0.07,
-                            onPressed: () {},
-                            icon: Icon(Icons.mode_comment_outlined)),
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: Text.rich(
-                          TextSpan(
-                            text: "40",
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
+                        padding:
+                            EdgeInsets.only(left: widget.size.width * 0.048),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              color: Colors.red,
+                              height: widget.size.height * 0.03,
+                              width: widget.size.width * 0.3,
                             ),
-                            children: [
-                              TextSpan(
-                                text: " comentários",
-                                style: TextStyle(
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onBackground,
-                                ),
-                              ),
-                            ],
+                            SizedBox(
+                              width: widget.size.width * 0.1,
+                            ),
+                            Container(
+                              color: Colors.red,
+                              height: widget.size.height * 0.03,
+                              width: widget.size.width * 0.4,
+                            ),
+                          ],
+                        ),
+                      )
+                    ]),
+                  )
+                : Column(
+                    children: [
+                      Padding(
+                        padding:
+                            EdgeInsets.only(left: widget.size.width * 0.01),
+                        child: ListTile(
+                          leading: InkWell(
+                            onTap: () {},
+                            child: Container(
+                              width: widget.size.width * 0.13,
+                              height: widget.size.width * 0.13,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                        "lib/ui/assets/images/test.jpg"),
+                                  )),
+                            ),
+                          ),
+                          title: Padding(
+                            padding: EdgeInsets.only(
+                                bottom: widget.size.width * 0.018),
+                            child: Text(
+                              "Pabricio Freitas",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          subtitle: Text("Terça-feira ás 08:22"),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: widget.size.height * 0.005,
+                          horizontal: widget.size.width * 0.05,
+                        ),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Text(
+                            "Mais um trabalho realizado com sucesso. Obrigado ao aplicativo por proporcionar essa oportunidade. ",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.left,
                           ),
                         ),
                       ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: widget.size.width * 0.015,
+                        ),
+                        child: Row(
+                          children: [
+                            IconButton(
+                              iconSize: widget.size.width * 0.07,
+                              onPressed: () {},
+                              icon: onPressedFavorite
+                                  ? Icon(
+                                      Icons.favorite_outlined,
+                                      color:
+                                          Theme.of(context).colorScheme.error,
+                                    )
+                                  : Icon(
+                                      Icons.favorite_border_outlined,
+                                    ),
+                            ),
+                            InkWell(
+                              onTap: () {},
+                              child: Text.rich(
+                                TextSpan(
+                                  text: "40",
+                                  style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  ),
+                                  children: [
+                                    TextSpan(
+                                      text: " curtidas",
+                                      style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onBackground,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            //
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: widget.size.width * 0.05,
+                              ),
+                              child: IconButton(
+                                  iconSize: widget.size.width * 0.07,
+                                  onPressed: () {},
+                                  icon: Icon(Icons.mode_comment_outlined)),
+                            ),
+                            InkWell(
+                              onTap: () {},
+                              child: Text.rich(
+                                TextSpan(
+                                  text: "40",
+                                  style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  ),
+                                  children: [
+                                    TextSpan(
+                                      text: " comentários",
+                                      style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onBackground,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
                     ],
                   ),
-                )
-              ],
-            ),
           ),
         ),
       ),
