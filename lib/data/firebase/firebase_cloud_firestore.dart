fe221506c336931b446c829f1bf6ff1158e8ead6
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-05 19:58:50 -0300

    feat: added firebasecloud functions

--- lib/data/firebase/firebase_cloud_firestore.dart
+++ lib/data/firebase/firebase_cloud_firestore.dart
@@ -1,9 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../../domain/entities/entities.dart';
+
 abstract class FirebaseCloudFirestore {
   CollectionReference getCollection({required String collectionName});
   Future<void> setDataDocument({required String doc, required Object data});
   Stream<QuerySnapshot> getStreamCollection({required String collectionName});
   Future<CollectionReference> getCollectionReference({required String collectionPath});
-  DocumentReference<Map<String, dynamic>> getPublishesByUserID({required String userId, required String publishId});
+  CollectionReference<Map<String, dynamic>> getPublishesCollectionRefByUserID({required String userId});
+  DocumentReference<Map<String, dynamic>> getPublisheDocumentByUserIdAndPublishId({required String userId, required String publishId});
+  Future<List<CommentEntity>> getCommentsByUserIdAndPublishId({required String userId, required String publishId});
+  Future<List<String>> getUsersUID();
 }
