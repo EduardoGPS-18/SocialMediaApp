05a23a36086daa2a4597523e5c78defbaebf98fe
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-05 11:54:30 -0300

    feat: create add publish impl

---
+++ lib/data/usecases/publish/remote_add_publish.dart
@@ -0,0 +1,9 @@
+import '../../../domain/entities/publish_entity.dart';
+import '../../../domain/usecases/usecases.dart';
+
+class RemoteAddPublish implements AddPublish {
+  @override
+  Future<void> addPublish({required PublishEntity publish}) {
+    throw UnimplementedError();
+  }
+}
