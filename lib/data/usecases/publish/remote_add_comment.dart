7c88096190952d678c003969c34a5956f1ad1ede
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-05 16:43:51 -0300

    feat: implemented remote add comment

--- lib/data/usecases/publish/remote_add_comment.dart
+++ lib/data/usecases/publish/remote_add_comment.dart
@@ -1,7 +1,7 @@
-import '../../models/models.dart';
-import '../../firebase/firebase.dart';
 import '../../../domain/entities/entities.dart';
 import '../../../domain/usecases/usecases.dart';
+import '../../firebase/firebase.dart';
+import '../../models/models.dart';
 
 class RemoteAddComment implements AddComment {
   FirebaseCloudFirestore firebaseCloudFirestore;
@@ -14,9 +14,15 @@ class RemoteAddComment implements AddComment {
 
   @override
   Future<void> addComment({required CommentEntity comment}) async {
-    final response = await firebaseCloudFirestore
-        .getCollection(collectionName: 'publishes')
-        .doc(comment.publishId)
-        .set({'comments': RemoteCommentModel.fromEntity(comment).toMap()});
+    final response = firebaseCloudFirestore.getPublishesByUserID(userId: comment.userId, publishId: comment.publishId);
+    final data = (await response.get()).data();
+
+    if (data == null) return;
+
+    final comments = data["comments"];
+
+    response.update({
+      "comments": [...comments, RemoteCommentModel.fromEntity(comment).toMap()]
+    });
   }
 }
