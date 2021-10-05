86f606ee8f1e88c379f5d75fa553e655b8500934
Pabricio Freitas <pabricio2000@gmail.com>
2021-10-05 16:40:18 -0300

    feat:added  text field blur function

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -15,64 +15,69 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: CustomAppBar(text: "Feed"),
-      body: PageView(
-        physics: const NeverScrollableScrollPhysics(),
-        controller: PageController(initialPage: 0),
-        children: [
-          SingleChildScrollView(
-            primary: true,
-            child: Column(children: [
-              SizedBox(
-                  height: size.height * 0.15,
-                  child: Post(
-                    image: "lib/ui/assets/images/test.jpg",
-                    hintTextTextField: "Adicione uma postagem",
-                    functionBottonTextField: () {},
-                    functionImage: () {},
-                    size: size,
-                  )),
-              CustomDivider(
-                size: size,
-                altura: 0.015,
-              ),
-              ListView.builder(
-                  primary: false,
-                  shrinkWrap: true,
-                  itemCount: 3,
-                  itemBuilder: (context, index) {
-                    return ViewPost(
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: CustomAppBar(text: "Feed"),
+        body: PageView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: PageController(initialPage: 0),
+          children: [
+            SingleChildScrollView(
+              primary: true,
+              child: Column(children: [
+                SizedBox(
+                    height: size.height * 0.15,
+                    child: Post(
+                      image: "lib/ui/assets/images/test.jpg",
+                      hintTextTextField: "Adicione uma postagem",
+                      functionBottonTextField: () {},
+                      functionImage: () {},
                       size: size,
-                    );
-                  }),
-            ]),
-          )
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 15,
-        backgroundColor: Theme.of(context).colorScheme.background,
-        onTap: (index) {
-          setState(() {
-            _currentIndexBottomNavigationBar = index;
-          });
-        },
-        currentIndex: _currentIndexBottomNavigationBar,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Feed',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
-            label: 'Postar',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Perfil',
-          ),
-        ],
+                    )),
+                CustomDivider(
+                  size: size,
+                  altura: 0.015,
+                ),
+                ListView.builder(
+                    primary: false,
+                    shrinkWrap: true,
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return ViewPost(
+                        size: size,
+                      );
+                    }),
+              ]),
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          elevation: 15,
+          backgroundColor: Theme.of(context).colorScheme.background,
+          onTap: (index) {
+            setState(() {
+              _currentIndexBottomNavigationBar = index;
+            });
+          },
+          currentIndex: _currentIndexBottomNavigationBar,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Feed',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline),
+              label: 'Postar',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Perfil',
+            ),
+          ],
+        ),
       ),
     );
   }
