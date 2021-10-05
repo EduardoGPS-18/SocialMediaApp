5145dfa1cb20bcaf5d040c1152652dee78fd5dcc
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-05 11:54:58 -0300

    feat: export data/usecases/publishes

---
+++ lib/data/usecases/publish/publish.dart
@@ -0,0 +1,5 @@
+export 'remote_add_comment.dart';
+export 'remote_add_publish.dart';
+export 'remote_load_publish_by_id.dart';
+export 'remote_load_publishes_by_uid.dart';
+export 'remote_load_recent_published.dart';
