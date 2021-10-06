72d8c879eaed8982c65d3910e3a43984d3173e83
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-06 14:49:24 -0300

    refactor: create type to delete comment

--- lib/domain/usecases/publish/delete_comment.dart
+++ lib/domain/usecases/publish/delete_comment.dart
@@ -1,3 +1,13 @@
 abstract class DeleteComment {
-  Future<void> deleteComment({required String commentId});
+  Future<void> deleteComment({required DeleteCommentParams params});
+}
+
+class DeleteCommentParams {
+  final String commentId;
+  final String publishId;
+
+  DeleteCommentParams({
+    required this.commentId,
+    required this.publishId,
+  });
 }
