807c0258829dc4b6bfc576ef9f56fa1f0ab7b147
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-06 13:33:02 -0300

    refactor: reimplemented get publishes by user id usecase

--- lib/data/usecases/publish/remote_load_publishes_by_user_id.dart
+++ lib/data/usecases/publish/remote_load_publishes_by_user_id.dart
@@ -1,7 +1,6 @@
 import '../../../domain/entities/publish_entity.dart';
 import '../../../domain/usecases/usecases.dart';
 import '../../firebase/firebase.dart';
-import '../../models/models.dart';
 
 class RemoteLoadPublishesByUserID implements LoadPublishesByUserID {
   FirebaseCloudFirestore firebaseCloudFirestore;
@@ -14,11 +13,9 @@ class RemoteLoadPublishesByUserID implements LoadPublishesByUserID {
 
   @override
   Future<List<PublishEntity>> getPublishesByUserID({required String userId}) async {
-    final response = firebaseCloudFirestore.getPublishesCollectionRefByUserID(userId: userId);
+    final response = await firebaseCloudFirestore.getPublishes();
+    final publishes = response.where((element) => element.userId == userId).toList();
 
-    final publishes = await response.get();
-
-    final list = publishes.docs.map((element) => RemotePublishModel.fromMap(element.data()).toEntity()).toList();
-    return list;
+    return publishes;
   }
 }
