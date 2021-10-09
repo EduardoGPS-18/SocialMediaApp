ad5821e946a424849ef93436c48d8f54173d5484
Guilherme Eler <50060454+GuiEler@users.noreply.github.com>
2021-10-09 12:33:51 -0300

    fix: treated null data

--- lib/ui/pages/post/post_page.dart
+++ lib/ui/pages/post/post_page.dart
@@ -27,96 +27,98 @@ class _PostPageState extends State<PostPage> {
     return StreamBuilder<PublishEntity>(
       stream: widget.presenter.getPublishById(id: publishId),
       builder: (context, publishSnapshot) {
-        return GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: Scaffold(
-            appBar: const CustomAppBar(text: "Postagem"),
-            body: SafeArea(
-              child: SizedBox.expand(
-                child: Column(
-                  children: [
-                    ViewPost(
-                      size: size,
-                      currentUser: widget.presenter.currentUser,
-                      publish: publishSnapshot.data!,
-                      publishUser: widget.presenter
-                          .loadUserById(id: publishSnapshot.data!.userId),
-                    ),
-                    CustomDivider(height: 0.002, size: size),
-                    StreamBuilder<List<CommentEntity>>(
-                      stream: widget.presenter.comments(publishId: publishId),
-                      builder: (context, commentsSnapshot) {
-                        return Expanded(
-                          child: ListView.separated(
-                            primary: false,
-                            shrinkWrap: true,
-                            itemCount: commentsSnapshot.data?.length ?? 0,
-                            separatorBuilder:
-                                (BuildContext context, int index) =>
-                                    CustomDivider(
-                              height: 0.002,
-                              size: size,
-                            ),
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: EdgeInsets.symmetric(
-                                  vertical: size.width * 0.03,
-                                  horizontal: size.width * 0.04,
-                                ),
-                                child: Comment(
+        if (publishSnapshot.hasData) {
+          return GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: Scaffold(
+              appBar: const CustomAppBar(text: "Postagem"),
+              body: SafeArea(
+                child: SizedBox.expand(
+                  child: Column(
+                    children: [
+                      ViewPost(
+                        size: size,
+                        currentUser: widget.presenter.currentUser,
+                        publish: publishSnapshot.data!,
+                        publishUser: widget.presenter
+                            .loadUserById(id: publishSnapshot.data!.userId),
+                      ),
+                      CustomDivider(height: 0.002, size: size),
+                      StreamBuilder<List<CommentEntity>>(
+                        stream: widget.presenter.comments(publishId: publishId),
+                        builder: (context, commentsSnapshot) {
+                          if (commentsSnapshot.hasData) {
+                            return Expanded(
+                              child: ListView.separated(
+                                primary: false,
+                                shrinkWrap: true,
+                                itemCount: commentsSnapshot.data?.length ?? 0,
+                                separatorBuilder:
+                                    (BuildContext context, int index) =>
+                                        CustomDivider(
+                                  height: 0.005,
                                   size: size,
-                                  onUserImageClick: () {},
-                                  user: widget.presenter.loadUserById(
-                                      id: commentsSnapshot
-                                              .data?[index].userId ??
-                                          ""),
-                                  commentContent:
-                                      commentsSnapshot.data?[index].content ??
-                                          "Sem conteudo!",
-                                  commentDate:
-                                      commentsSnapshot.data?[index].createdAt ??
-                                          DateTime.now(),
                                 ),
+                                itemBuilder: (context, index) {
+                                  return Comment(
+                                    size: size,
+                                    onUserImageClick: () {},
+                                    user: widget.presenter.loadUserById(
+                                        id: commentsSnapshot
+                                                .data?[index].userId ??
+                                            ""),
+                                    commentContent:
+                                        commentsSnapshot.data?[index].content ??
+                                            "Sem conteudo!",
+                                    commentDate: commentsSnapshot
+                                            .data?[index].createdAt ??
+                                        DateTime.now(),
+                                  );
+                                },
+                              ),
+                            );
+                          } else {
+                            return const SizedBox();
+                          }
+                        },
+                      ),
+                      CustomDivider(height: 0.002, size: size),
+                      StreamBuilder<UserEntity>(
+                        stream: widget.presenter.currentUser,
+                        builder: (context, userSnapshot) {
+                          return StreamBuilder<bool>(
+                            stream: widget.presenter.isValidComment,
+                            builder: (context, isValidSnapshot) {
+                              return Post(
+                                image: userSnapshot.data?.photoUrl ?? "",
+                                hintTextTextField: "Adicione uma comentário",
+                                functionBottonTextField: () {},
+                                functionImage: () {},
+                                onTextEditing: widget.presenter.validateComment,
+                                onEditingComplete: isValidSnapshot.hasData &&
+                                        isValidSnapshot.data == true
+                                    ? () async => await widget.presenter
+                                        .addComment(
+                                            publishId:
+                                                publishSnapshot.data?.uid ?? "")
+                                    : null,
+                                size: size,
                               );
                             },
-                          ),
-                        );
-                      },
-                    ),
-                    CustomDivider(height: 0.002, size: size),
-                    StreamBuilder<UserEntity>(
-                      stream: widget.presenter.currentUser,
-                      builder: (context, userSnapshot) {
-                        return StreamBuilder<bool>(
-                          stream: widget.presenter.isValidComment,
-                          builder: (context, isValidSnapshot) {
-                            return Post(
-                              image: userSnapshot.data?.photoUrl ?? "",
-                              hintTextTextField: "Adicione uma comentário",
-                              functionBottonTextField: () {},
-                              functionImage: () {},
-                              onTextEditing: widget.presenter.validateComment,
-                              onEditingComplete: isValidSnapshot.hasData &&
-                                      isValidSnapshot.data == true
-                                  ? () async => await widget.presenter
-                                      .addComment(
-                                          publishId:
-                                              publishSnapshot.data?.uid ?? "")
-                                  : null,
-                              size: size,
-                            );
-                          },
-                        );
-                      },
-                    ),
-                  ],
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        );
+          );
+        } else {
+          return const SizedBox();
+        }
       },
     );
   }
