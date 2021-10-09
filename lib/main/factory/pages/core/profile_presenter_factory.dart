657539da89315b5a9704021d234c51bb25240c51
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-09 00:24:36 -0300

    feat: added save user image factory

--- lib/main/factory/pages/core/profile_presenter_factory.dart
+++ lib/main/factory/pages/core/profile_presenter_factory.dart
@@ -1,12 +1,14 @@
-import 'package:social_media_app/main/factory/factory.dart';
-import 'package:social_media_app/main/factory/pages/core/profile_validation_factory.dart';
 import '../../../../presentation/presenters/presenters.dart';
-
 import '../../../../ui/pages/pages.dart';
+import '../../factory.dart';
+import '../../usecases/saver_user_image_factory.dart';
+import 'profile_validation_factory.dart';
 
 ProfilePresenter makeProfilePresenter() => GetxProfilePresenter(
-    validation: makeProfileValidation(),
-    localGetImage: makeSelectLocalImage(),
-    localGetUserId: makeLocalGetUserId(),
-    remoteGetPublishesByUserID: makeLoadPublishesByUserId(),
-    remoteLoadUser: makeLoadUser());
+      validation: makeProfileValidation(),
+      localGetImage: makeSelectLocalImage(),
+      localGetUserId: makeLocalGetUserId(),
+      remoteGetPublishesByUserID: makeLoadPublishesByUserId(),
+      remoteLoadUser: makeLoadUser(),
+      remoteSaveUserImage: makeSaveUserImage(),
+    );
