ac4d6f754336b109c4fceb277f288cb1504984f0
Eduardo Silva <64669750+EduardoGPS-18@users.noreply.github.com>
2021-10-09 23:54:15 -0300

    feat: implement logout and renemed property

--- lib/main/factory/pages/core/core_page_factory.dart
+++ lib/main/factory/pages/core/core_page_factory.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+
 import '../../../../ui/pages/pages.dart';
 import 'core.dart';
 
 Widget makeCorePage() => CorePageApp(
-      presenter: makeCorePresenter(),
+      corePresenter: makeCorePresenter(),
       feedPresenter: makeFeedPresenter(),
       profilePresenter: makeProfilePresenter(),
     );
--- lib/main/factory/pages/core/core_presenter_factory.dart
+++ lib/main/factory/pages/core/core_presenter_factory.dart
@@ -2,4 +2,8 @@ import '../../../../presentation/presenters/presenters.dart';
 import '../../../../ui/pages/pages.dart';
 import '../../factory.dart';
 
-CorePresenter makeCorePresenter() => GetxCorePresenter(remoteLogout: makeLogout());
+CorePresenter makeCorePresenter() => GetxCorePresenter(
+      remoteLogout: makeLogout(),
+      localGetUserId: makeLocalGetUserId(),
+      remoteLoadUser: makeLoadUser(),
+    );
